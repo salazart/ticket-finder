474afcaa06db72a5236cdb351acdd20a897be8c5
Alexandr <s.o.w@i.ua>
2018-12-01 14:52:27 +0200

    Initial commit

---
+++ src/test/java/com/sz/tf/TicketFinderApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sz.tf;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TicketFinderApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
